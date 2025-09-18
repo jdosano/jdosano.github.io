1be15e0274430d6d0474ad187e35df1608c213df
jdosano <jdowdy@osano.com>
2025-09-18 14:44:29 -0400

    add analytics and essential cookies

--- scripts/essential-tester.js
+++ scripts/essential-tester.js
@@ -1,1 +1,3 @@
-// Nothing to see here. Just something to categorize as essential
+console.log("Essential Script has loaded.")
+setCookie("analytics_from_essential_script", "im-analytics", 7); // Cookie lasts 7 days
+setCookie("analytics_from_essential_script", "im-essential", 7); // Cookie lasts 7 days
